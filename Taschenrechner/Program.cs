10cd4943f24b0711f2c5f464aa7fb17fe136f671
Nicolas <ni_ha@bluewin.ch>
2021-01-26 09:14:49 +0100

    Commit1

---
+++ Taschenrechner/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Taschenrechner
+{
+    class Program
+    {
+
+        static double Fakultaet( int basis)
+        {
+            double ret = 1.0;
+            double factor = 1.0;
+
+            for( int cnt = 0; cnt< basis; cnt++)
+            {
+                ret = ret * factor;
+                factor = factor + 1.0;
+            }
+
+            return ret;
+        }
+
+        static double SquareRoot( double value)
+        {
+            throw new NotImplementedException();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(Fakultaet(5));
+        }
+    }
+}
---
+++ Taschenrechner2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Taschenrechner2
+{
+    class Program
+    {
+
+        static double fakultaet(int basis)
+        {
+            double ret = 1.0;
+            double faktor = 1.0;
+
+            for (int count = 0; count < basis; count++)
+            {
+                ret = ret * faktor;
+                faktor = faktor + 1.0;
+            }
+
+            return ret;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(fakultaet(7));
+        }
+    }
+}
